ffeb898c4d2a164269e5d53a88990b945dc21b9d
Ivan Titov <risenforce@mail.ru>
2019-04-21 13:35:29 +0500

    Merge 17.5.228

--- examples/nodes/dop/scriptsolver/support_files/FreezeSetup.cmd
+++
@@ -1,9 +0,0 @@
-
-set lastobject=`dopsolvenumobjects() - 1`
-for i = 0 to $lastobject
-    dopsolveadddata $i CustomData SIM_EmptyData
-    # Initialize the moved value to zero.
-    dopsolvesetoption -t bool $i CustomData moved false
-    # Initialize the active value to one.
-    dopsolvesetoption $i SolverParms/ActiveValue active 1
-end
